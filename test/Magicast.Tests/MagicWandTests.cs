5338603864ddfc77496e432e249114f21d08b90c
Philip Panyukov <ppanyukov@googlemail.com>
2015-07-27 10:46:13 +0100

    Test: Add tests for class hierarchies.

--- test/Magicast.Tests/MagicWandTests.cs
+++ test/Magicast.Tests/MagicWandTests.cs
@@ -3,7 +3,6 @@
 namespace Magicast.Tests
 {
     using System;
-    using System.Runtime.InteropServices;
 
     using Xunit;
 
@@ -67,8 +66,83 @@ namespace Magicast.Tests
             Assert.Throws<InvalidOperationException>(() => MagicWand<FooClass, BarStruct>.Cast(foo));
         }
 
+        [Fact]
+        public void CanCastClassHierarchyToFlatClass()
+        {
+            // Having the source class FooDerivedClass which is part of the hierarchy:
+            //    object 
+            //      --> FooBaseClass 
+            //              fieldA
+            //              fieldB
+            //          --> FooDerivedClass
+            //              fieldC
+            //              fieldD
+            //
+            // We can cast it to another "flat" class and accumulate all the fields:
+            //      object
+            //      --> BarFlatClass
+            //          fieldA
+            //          fieldB
+            //          fieldC
+            //          fieldD
+            var fooDerived = new FooDerivedClass
+            {
+                fieldBaseA = "fieldBaseA",
+                fieldBaseB = "fieldBaseB",
+                fieldDerivedA = "fieldDerivedA",
+                fieldDerivedB = "fieldDerivedB"
+            };
+
+            var bar = MagicWand<FooDerivedClass, BarFlatClass>.Cast(fooDerived);
+            Assert.Equal(fooDerived.fieldBaseA, bar.fieldFlatA);
+            Assert.Equal(fooDerived.fieldBaseB, bar.fieldFlatB);
+            Assert.Equal(fooDerived.fieldDerivedA, bar.fieldFlatC);
+            Assert.Equal(fooDerived.fieldDerivedB, bar.fieldFlatD);
+        }
+
+        [Fact]
+        public void CanCastFlatClassToClassHierarchy()
+        {
+            // Just like we can cast source type with a class hierarchy to flat type,
+            // we can do the same the other way around:
+            // Cast a type with flat structure to hierarchy:
+            //
+            // Having the source flat class FooFlatClass:
+            //      object
+            //      --> FooFlatClass
+            //          fieldA
+            //          fieldB
+            //          fieldC
+            //          fieldD
+            //
+            // We can cast it to a type with which is part of hierarchy:
+            //    object 
+            //      --> FooBaseClass 
+            //              fieldA
+            //              fieldB
+            //          --> FooDerivedClass
+            //              fieldC
+            //              fieldD
+            //
+
+            var barFlat = new BarFlatClass
+            {
+                fieldFlatA = "fieldBaseA",
+                fieldFlatB = "fieldBaseB",
+                fieldFlatC = "fieldBaseC",
+                fieldFlatD = "fieldBaseD",
+            };
+
+            var fooHierarchy = MagicWand<BarFlatClass, FooDerivedClass>.Cast(barFlat);
+            Assert.Equal(barFlat.fieldFlatA, fooHierarchy.fieldBaseA);
+            Assert.Equal(barFlat.fieldFlatB, fooHierarchy.fieldBaseB);
+            Assert.Equal(barFlat.fieldFlatC, fooHierarchy.fieldDerivedA);
+            Assert.Equal(barFlat.fieldFlatD, fooHierarchy.fieldDerivedB);
+        }
 
         // Types under test
+
+        // Simple structs with same structure
         public struct FooStruct
         {
             public string fieldA;
@@ -81,6 +155,7 @@ namespace Magicast.Tests
             public string fieldD;
         }
 
+        // Simple classes with same structure
         public class FooClass
         {
             public string fieldA;
@@ -91,6 +166,31 @@ namespace Magicast.Tests
         {
             public string fieldC;
             public string fieldD;
+        }
+
+
+        // Class hierarchy
+
+        // The base and the derived classes
+        public class FooBaseClass
+        {
+            public string fieldBaseA;
+            public string fieldBaseB;
+        }
+
+        public class FooDerivedClass : FooBaseClass
+        {
+            public string fieldDerivedA;
+            public string fieldDerivedB;
+        }
+
+        // This one combines the types form hierarchies in layout.
+        public class BarFlatClass
+        {
+            public string fieldFlatA;
+            public string fieldFlatB;
+            public string fieldFlatC;
+            public string fieldFlatD;
         }
     }
 }
